52f9ca758badac19e9b2b88961c4f9410fe7ac72
bilalpasta17 <83414545+bilalpasta17@users.noreply.github.com>
2023-06-12 16:16:18 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Next_Screen.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Next_Screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next_screen);
+    }
+}
